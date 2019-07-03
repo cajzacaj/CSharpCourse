140ae8a6420ca4debb3ce00e180cc195e469a68b
cajzacaj <cajzacaj@gmail.com>
2019-07-03 09:17:25 +0200

    Glömde lite shapes

---
+++ CSharpCourse/InheritenceShapes/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritenceShapes
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override double GetArea()
+        {
+            double pi = Math.PI;
+            double area = pi * Radius * Radius;
+
+            return area;
+        }
+    }
+}

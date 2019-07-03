55eb4bd2e599ac23e91f115914f75503cb3e0714
cajzacaj <cajzacaj@gmail.com>
2019-07-03 16:45:23 +0200

    Ändrat tester

--- CSharpCourse/TestShipping/UnitTest1.cs
+++ CSharpCourse/TestShipping/UnitTest1.cs
@@ -11,11 +11,11 @@ namespace TestShipping
         [DataRow(0)]
         [DataRow(25)]
         [DataRow(50)]
-        public void shipping_should_cost_88kr_for_0_to_50grams_recommended_letters_not_bulky(int weight)
+        public void shipping_should_cost_88kr_for_0_to_50grams_recommended_letters_not_bulky_2019(int weight)
         {
             var service = new ShippingService();
             var letter = new Letter(weight, true, false);
-            decimal fee = service.CalculateShipping(letter);
+            decimal fee = service.CalculateShipping(letter, "2019");
 
             Assert.AreEqual(88, fee);
         }
@@ -24,11 +24,23 @@ namespace TestShipping
         [DataRow(251)]
         [DataRow(400)]
         [DataRow(500)]
-        public void shipping_should_cost_54kr_for_251_to_500grams_regular_letters_not_bulky(int weight)
+        public void shipping_should_cost_54kr_for_251_to_500grams_regular_letters_not_bulky1_2019(int weight)
         {
             var service = new ShippingService();
             var letter = new Letter(weight, false, false);
-            decimal fee = service.CalculateShipping(letter);
+            decimal fee = service.CalculateShipping(letter, "2019");
+
+            Assert.AreEqual(54, fee);
+        }
+        [TestMethod]
+        [DataRow(251)]
+        [DataRow(400)]
+        [DataRow(500)]
+        public void shipping_should_cost_54kr_for_251_to_500grams_regular_letters_not_bulky1_2018(int weight)
+        {
+            var service = new ShippingService();
+            var letter = new Letter(weight, false, false);
+            decimal fee = service.CalculateShipping(letter, "2019");
 
             Assert.AreEqual(54, fee);
         }

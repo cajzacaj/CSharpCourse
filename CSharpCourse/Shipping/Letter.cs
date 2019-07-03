6761475375ca447ec92c7d03757521de03fb981c
cajzacaj <cajzacaj@gmail.com>
2019-07-03 19:24:38 +0200

    Uppdaterade shipping

--- CSharpCourse/Shipping/Letter.cs
+++ CSharpCourse/Shipping/Letter.cs
@@ -6,15 +6,15 @@ namespace shipping
 {
     public class Letter
     {
-        public bool RecommendedLetter { get; set; }
-        public bool Bulky { get; set; }
-        public int Weight { get; set; }
+        public bool RecommendedLetter { get; }
+        public bool Bulky { get; }
+        public int Weight { get; }
 
         public Letter(int weigthInGrams, bool isRecommendedLetter, bool isBulky)
         {
             if (weigthInGrams < 0)
                 throw new ArgumentException("A letter can't have a negative weight");
-            if (weigthInGrams > 2000)
+            else if (weigthInGrams > 2000)
                 throw new ArgumentException("The letter is too heavy, please send as parcel");
             else
                 Weight = weigthInGrams;
--- CSharpCourse/Shipping/Program.cs
+++ CSharpCourse/Shipping/Program.cs
@@ -1,14 +1,48 @@
 ﻿using System;
+using CSharpCourse.Utilities;
 
 namespace shipping
 {
     class Program
     {
+        static ConsoleHelper ch = new ConsoleHelper();
         static void Main(string[] args)
         {
             var service = new ShippingService();
-            var letter = new Letter(254, false, false);
+            Letter letter;
 
+            while (true)
+            {
+                try
+                {
+                    bool rek = false;
+                    bool bulky = false;
+
+                    int weight = ch.AskForInteger("Enter weight:");
+
+                    string input = ch.AskForString("Recommended (y/n)");
+                    if (input == "y")
+                        rek = true;
+
+                    input = ch.AskForString("Bulky (y/n)");
+                    if (input == "y")
+                        bulky = true;
+
+                    letter = new Letter(weight, rek, bulky);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                break;
+            }
+            Console.WriteLine();
+            Console.WriteLine(letter.Weight);
+            Console.WriteLine(letter.RecommendedLetter);
+            Console.WriteLine(letter.Bulky);
+
+            Console.WriteLine();
             decimal shipping = service.CalculateShipping(letter, "2019");
             Console.WriteLine(shipping);
         }

7657fd2a45b70ac4644c0c95d9bf2072d303f5b2
cajzacaj <cajzacaj@gmail.com>
2019-07-04 11:51:47 +0200

    Löst alla test

--- CSharpCourse/MethodsAndLists/MethodsAndLists.Core/NumberListToString.cs
+++ CSharpCourse/MethodsAndLists/MethodsAndLists.Core/NumberListToString.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MethodsAndLists.Core
 {
@@ -7,12 +8,29 @@ namespace MethodsAndLists.Core
     {
         public string ReportFirstAndLastValue(List<int> list)
         {
-            throw new NotImplementedException();
+            if(list?.Any() != true)
+                throw new ArgumentException("List is null or empty");
+
+            return $"Första siffran är {list[0]} och sista siffran är {list[list.Count - 1]}";
         }
 
         public string ReportNumberOfNegativeValues(List<int> list)
         {
-            throw new NotImplementedException();
+            var negativeNums = new List<int>();
+
+            foreach (int i in list)
+            {
+                if (i < 0)
+                    negativeNums.Add(i);
+            }
+
+            if (negativeNums.Count == 0)
+                return "Jippi! Det finns inga negativa tal i listan";
+            else if (negativeNums.Count == 1)
+                return "Det finns ett negativt tal i listan";
+            else 
+                return $"Det finns {negativeNums.Count} st negativa tal i listan";
+
         }
     }
 }

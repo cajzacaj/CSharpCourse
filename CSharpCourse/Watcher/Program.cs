dfb83fd33c1993b375efaed7c75ef20bee0efd23
cajzacaj <cajzacaj@gmail.com>
2019-07-10 11:25:29 +0200

    First commit - Watcher

---
+++ CSharpCourse/Watcher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Watcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watcher = new FileSystemWatcher();
+            watcher.Path = @"C:\TMP";
+            watcher.EnableRaisingEvents = true;
+
+            watcher.Created += Watcher_Created;
+            watcher.Renamed += Watcher_Renamed;
+            watcher.Deleted += Watcher_Deleted;
+
+            Console.ReadKey();
+        }
+
+        private static void Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.Name} was deleted");
+        }
+
+        private static void Watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"{e.Name} was renamed");
+        }
+
+        private static void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.Name} was created");
+        }
+
+        private static void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.Name} was changed");
+        }
+    }
+}

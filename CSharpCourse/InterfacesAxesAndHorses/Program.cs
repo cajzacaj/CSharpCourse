b5e8461abb056d03e0ed58d5cb7cdfbf28d00574
cajzacaj <cajzacaj@gmail.com>
2019-07-10 10:09:43 +0200

    First commit - Interfacesövningar - klar

---
+++ CSharpCourse/InterfacesAxesAndHorses/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+
+namespace InterfacesAxesAndHorses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greet(new Animal());
+            //Greet(new Axe());
+            //Greet(new Bread());
+            Greet(new Horse());
+            Greet(new Mouse());
+            //Greet(new Sword());
+
+            //PutInBag(new Animal());
+            PutInBag(new Axe());
+            PutInBag(new Bread());
+            //PutInBag(new Horse());
+            PutInBag(new Mouse());
+            PutInBag(new Sword());
+
+            //DoDamageWith(new Animal());
+            DoDamageWith(new Axe());
+            //DoDamageWith(new Bread());
+            DoDamageWith(new Horse());
+            //DoDamageWith(new Mouse());
+            DoDamageWith(new Sword());
+        }
+
+        private static void DoDamageWith(IDangerous x)
+        {
+            x.DoDamage();
+        }
+
+        private static void PutInBag(IPackable x)
+        {
+            x.Pack();
+        }
+
+        private static void Greet(Animal animal)
+        {
+            animal.SayHello();
+        }
+    }
+
+    interface IDangerous
+    {
+        void DoDamage();
+    }
+    interface IPackable
+    {
+        void Pack();
+    }
+    class Animal
+    {
+        public void SayHello()
+        {
+        }
+    }
+    class Mouse : Animal, IPackable
+    {
+        public void Pack()
+        {
+
+        }
+    }
+    class Horse : Animal, IDangerous
+    {
+        public void DoDamage()
+        {
+        }
+    }
+    class Axe : IPackable, IDangerous
+    {
+        public void DoDamage()
+        {
+        }
+
+        public void Pack()
+        {
+        }
+    }
+    class Sword : IPackable, IDangerous
+    {
+        public void DoDamage()
+        {
+        }
+
+        public void Pack()
+        {
+        }
+    }
+    class Bread : IPackable
+    {
+        public void Pack()
+        {
+        }
+    }
+}
---
+++ CSharpCourse/InterfacesRockstars/Program.cs
@@ -0,0 +1,51 @@
+﻿using CSharpCourse.Utilities;
+using System;
+using System.Collections.Generic;
+
+namespace InterfacesRockstars
+{
+    class Program
+    {
+        static ConsoleHelper ch = new ConsoleHelper();
+        static void Main(string[] args)
+        {
+            string[] rockstarsArray = new string[] { "Jim Morrison", "Ozzy Osborne", "David Bowie", "Freddie Mercury" };
+            List<string> rockstarsList = new List<string> { "Jim Morrison", "Ozzy Osborne", "David Bowie", "Freddie Mercury" };
+
+            DisplayArrayOfRockStars(rockstarsArray);
+            ch.Space();
+            DisplayListOfRockStars(rockstarsList);
+            ch.Space();
+            DisplayIEnumerableOfRockStars(rockstarsArray);
+            ch.Space();
+            DisplayIEnumerableOfRockStars(rockstarsList);
+        }
+
+        private static void DisplayArrayOfRockStars(string[] rockstarsArray)
+        {
+            ch.WriteLine("My rockstars: (Array)");
+            foreach (var rockstar in rockstarsArray)
+            {
+                ch.WriteLineDark($"* {rockstar}");
+            }
+        }
+
+        private static void DisplayListOfRockStars(List<string> rockstarsList)
+        {
+            ch.WriteLine("My rockstars: (List)");
+            foreach (var rockstar in rockstarsList)
+            {
+                ch.WriteLineDark($"* {rockstar}");
+            }
+        }
+        private static void DisplayIEnumerableOfRockStars(IEnumerable<string> rockstarsList)
+        {
+            ch.WriteLine("My rockstars: (IEnumerable)");
+            foreach (var rockstar in rockstarsList)
+            {
+                ch.WriteLineDark($"* {rockstar}");
+            }
+
+        }
+    }
+}
